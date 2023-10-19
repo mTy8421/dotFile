654783caac35aef7a8f728ee428f03972c48a668
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2023-10-20 03:58:09 +0700

    Update anvim.sh

--- dotfiles/anvim.sh
+++ dotfiles/anvim.sh
@@ -32,11 +32,11 @@ sudo install lazygit /usr/local/bin
 
 curl -L https://github.com/dundee/gdu/releases/latest/download/gdu_linux_amd64.tgz | tar xz
 chmod +x gdu_linux_amd64
-mv gdu_linux_amd64 /usr/bin/gdu
+sudo mv gdu_linux_amd64 /usr/bin/gdu
 
-add-apt-repository ppa:daniel-milde/gdu
-apt-get update
-apt-get install gdu
+sudo add-apt-repository ppa:daniel-milde/gdu
+sudo apt-get update
+sudo apt-get install gdu
 
 # x86-64
 curl -LO https://github.com/ClementTsang/bottom/releases/download/0.9.6/bottom_0.9.6_amd64.deb
