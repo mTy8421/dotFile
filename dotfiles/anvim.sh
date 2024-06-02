fad41a729a62086a47e2ef84a4e777a023e63da6
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2024-06-03 00:58:33 +0700

    Update anvim.sh

--- dotfiles/anvim.sh
+++ dotfiles/anvim.sh
@@ -1,14 +1,15 @@
 ### Install Node js
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+# installs nvm (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+# download and install Node.js
+nvm install 20
 
-sudo apt-get update
-sudo apt-get install nodejs -y
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.14.0`
+
+# verifies the right NPM version is in the environment
+npm -v # should print `10.7.0`
 ####
 
 ### install neovim
