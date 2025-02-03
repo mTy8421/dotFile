1de7719c8d36917052ca035a9198dbe019d534ac
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2025-02-03 09:49:58 +0700

    Update anvim.sh

--- dotfiles/anvim.sh
+++ dotfiles/anvim.sh
@@ -16,7 +16,11 @@ npm -v # should print `10.7.0`
 ####
 
 ### install neovim
-sudo snap install nvim --classic
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
 ###
 
 ### install tree-sitter
