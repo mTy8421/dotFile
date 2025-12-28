3b5dad44f4dd8fe731e09eb495f1c64ddb919983
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2025-12-28 13:01:15 +0700

    Update anvim.sh

--- dotfiles/anvim.sh
+++ dotfiles/anvim.sh
@@ -1,13 +1,5 @@
 ### Install Node js
 
-### install neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
-
-export PATH="$PATH:/opt/nvim-linux-x86_64/bin"
-###
-
 ### install tree-sitter
 npm install -g tree-sitter-cli
 ###
