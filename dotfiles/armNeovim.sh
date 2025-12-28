6260a6569aa8c8a9c5989036c0a324d334b7975c
Panthakit totid <79712705+mTy8421@users.noreply.github.com>
2025-12-28 12:56:44 +0700

    Add ARM Neovim installation script
    This script installs Neovim for ARM architecture and updates the PATH in bash and zsh configuration files.

---
+++ dotfiles/armNeovim.sh
@@ -0,0 +1,7 @@
+## install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-arm64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+echo export PATH="$PATH:/opt/nvim-linux-x86_64/bin" >> ~/.bashrc
+echo export PATH="$PATH:/opt/nvim-linux-x86_64/bin" >> ~/.zshrc
